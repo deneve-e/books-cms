4e60b345755a9d62a6324896d8b95a48889ecb96
deneve <denevee@proton.me>
2024-07-29 14:40:08 +0300

    add the book service / resolver / type

--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -1,4 +1,51 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 
-@Resolver()
-export class BooksResolver {}
+import { BooksService } from './books.service';
+import { Book } from './book.entity';
+
+@Resolver((of) => Book)
+export class BooksResolver {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Query((returns) => [Book])
+  books() {
+    return this.booksService.findAll();
+  }
+
+  @Query((returns) => Book)
+  book(@Args('id') id: number) {
+    return this.booksService.findOne(id);
+  }
+
+  @Mutation((returns) => Book)
+  createBook(
+    @Args('title') title: string,
+    @Args('author') author: string,
+    @Args('publicationDate') publicationDate: Date,
+  ) {
+    const book = new Book();
+    book.title = title;
+    book.author = author;
+    book.publicationDate = publicationDate;
+    return this.booksService.create(book);
+  }
+
+  @Mutation((returns) => Book)
+  updateBook(
+    @Args('id') id: number,
+    @Args('title') title: string,
+    @Args('author') author: string,
+    @Args('publicationDate') publicationDate: Date,
+  ) {
+    const book = new Book();
+    book.title = title;
+    book.author = author;
+    book.publicationDate = publicationDate;
+    return this.booksService.update(id, book);
+  }
+
+  @Mutation((returns) => Boolean)
+  deleteBook(@Args('id') id: number) {
+    return this.booksService.remove(id);
+  }
+}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Book } from './book.entity';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(
+    @InjectRepository(Book)
+    private readonly bookRepository: Repository<Book>,
+  ) {}
+
+  create(book: Book): Promise<Book> {
+    return this.bookRepository.save(book);
+  }
+
+  findAll(): Promise<Book[]> {
+    return this.bookRepository.find();
+  }
+
+  findOne(id: number): Promise<Book> {
+    return this.bookRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async update(id: number, book: Book): Promise<void> {
+    await this.bookRepository.update(id, book);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.bookRepository.delete(id);
+  }
+}
