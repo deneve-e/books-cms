2a243aaa049600436ee326a59c71f0390bbe5bc5
deneve <denevee@proton.me>
2024-07-29 14:18:11 +0300

    generate auth  modules, services, resolvers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
