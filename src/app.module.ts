894f6918949701b12e3799bfc4b50909625f35b1
deneve <denevee@proton.me>
2024-07-29 14:21:44 +0300

    configure the TypeORM connection in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BooksModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'booksdb',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    BooksModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
